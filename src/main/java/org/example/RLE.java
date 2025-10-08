e1fce90eb7fbdf6394a2d9cf751e0d1373826e9f
Aydan Lau <aydanlau@gmail.com>
2025-10-08 10:00:58 +0800

    the test case somehow doesnt work even though its the same

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -26,11 +26,39 @@ public class RLE {
     }
 
     /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+    public static String compress(String uncompressed) {
+        StringBuilder compressed = new StringBuilder();
+
+        int count = 0;
+        char prev = uncompressed.charAt(0);
+        for (char c: uncompressed.toCharArray()) {
+            if (prev == c) {
+                count++;
+            } else {
+                compressed.append(count);
+                compressed.append(prev);
+                prev = c;
+                count = 1;
+            }
+        }
+
+        return compressed.toString();
+    }
 
     /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
     public static String decompress(String compressed) {
-        return null;
+        StringBuilder decompressed = new StringBuilder();
+
+        int count = -1;
+        for (int i = 0; i < compressed.length(); i++) {
+            if (i % 2 == 0) {
+                count = compressed.charAt(i) - '0';
+            } else {
+                decompressed.append(String.valueOf(compressed.charAt(i)).repeat(count));
+            }
+        }
+
+        return decompressed.toString();
     }
 
 
